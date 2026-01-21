ae9515a8c9adc0a9408b3b00bb2f028cc0bd403b
Italo Costa <italocostajobs@gmail.com>
2026-01-21 01:14:59 -0300

    first commit

---
+++ src/test/java/com/perinity/grc/infrastructure/inbound/rest/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.perinity.perinity.infrastructure.inbound.rest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
